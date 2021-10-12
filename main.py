c097f363ec37d8d72ff48dcf9a6728bae9630e2c
Lab_ldqsss <734200940@qq.com>
2021-10-12 21:35:40 +0800

    add  def insert_dev(self), 自动更新development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import re
 
 from fire.core import Fire
 from libs.mysql import Mysql
@@ -20,6 +21,7 @@ class Main:
         while True:
             try:
                 self.common_db = Mysql(COMMON_WEBSITE_CONFIG)
+                self.insert_dev()
                 for i in self.common_db.select(SQL_DEVELOPMENT_SELECT):
                     if i[1]+'.py' in os.listdir('crawler/v1') or i[1]+'.py' in os.listdir('crawler/pass'):
                         self.common_db.execute(SQL_DEVELOPMENT_TIME_UPDATE.format(DateUtil.time_now_formate(), i[1]))
@@ -32,7 +34,25 @@ class Main:
                 print("主进程出错 ==> {}".format(e))
                 self.common_db = None
 
-
+    def insert_dev(self):
+        passList=os.listdir('crawler/pass')
+        passList.remove("__init__.py")
+        if passList:
+            try:
+                deployedSpis =[i[0] for i in self.common_db.select(SQL_DEVELOPMENT_SPIDERNAME_SELECT)]
+                for i in passList:
+                    name = i[:-3]
+                    if name not in deployedSpis:
+                        spiderFile=open(file=f'crawler/pass/{i}',mode='r',encoding='utf-8').read()
+                        proxy =re.findall("proxy[ =']+\d+",spiderFile)[0] if re.findall("proxy[ =']+\d+",spiderFile) else '00'
+                        is_http = 1 if re.findall("is_http[ '=1]",spiderFile) else 0
+                        website_id = re.findall('\d+',re.findall("website_id[ =]+\d+", spiderFile)[0])[0]
+                        self.common_db.execute(SQL_DEVELOPMENT_INSERT.format(website_id, name, proxy, is_http))
+            except Exception as e:
+                print("Something wrong with the spiders in folder pass:",end='')
+                print(e)
+        else:
+            print("Accomplished Spiders Were Not Found")
 # 主进程函数
 # python -m main
 # python -m main --db=00 --time=now
