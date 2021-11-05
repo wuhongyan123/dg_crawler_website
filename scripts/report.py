3f43aef07559d4dbc9b062609569c9a6cbc552e4
Lab_ldqsss <734200940@qq.com>
2021-11-05 22:01:38 +0800

    add LinMing E-mail Addr

--- scripts/report.py
+++ scripts/report.py
@@ -7,7 +7,6 @@ from config.db_config import *
 from libs.excel import Excel
 from libs.mail import Mail
 
-
 """
 数据部 每日数据获取报表脚本。
 生成报表.xls文件
@@ -19,7 +18,7 @@ smtp = 'smtp.163.com'  # smtp域名
 # passwd = 'wxuhjbiaippcbbac'  # 授权码(ldq)
 sender = 'gdufs_shuwa@163.com'
 passwd = 'JYYXNIASHNOYBTLG'  # 授权码(gdufs_shuwa)
-receivers = [ # 收件人邮箱
+receivers = [  # 收件人邮箱
     'gdufs_shuwa@163.com',
     '657742829@qq.com',
     'jiangshengyi@163.com',
@@ -34,12 +33,12 @@ receivers = [ # 收件人邮箱
     '2754976781@qq.com',
     '1024463088@qq.com',
     '2905939881@qq.com',
-    '1095791132@qq.com', # 尹启熙
+    '1095791132@qq.com',  # 尹启熙
     '1197072098@qq.com',
-    '2630862330@qq.com', # WPF
-    'songyt0325@163.com', # SongYutao
+    '2630862330@qq.com',  # WPF
+    'songyt0325@163.com',  # SongYutao
+    '1226174058@qq.com'  # LinMing
 ]
-
 
 # sql语句,日常数据获取报告和日常获取量。
 daily_sql = "SELECT website_id,count(*) FROM news WHERE cole_time >= '{}' GROUP BY website_id ORDER BY NULL"
@@ -51,8 +50,10 @@ time_yesterday = time.strftime("%Y-%m-%d", time.localtime(time.time() - 86400))
 # 文件路径
 file = '{}.xls'
 # 累计数据获取报告和累计获取量。
-daily_fields_list = ['website_id', 'url', 'c_name', 'country', 'language', 'remark', 'developer', 'start_time', 'news_amount']
-all_fields_list= ['website_id', 'url', 'c_name', 'country', 'language', 'remark', 'developer', 'start_time', 'news_amount']
+daily_fields_list = ['website_id', 'url', 'c_name', 'country', 'language', 'remark', 'developer', 'start_time',
+                     'news_amount']
+all_fields_list = ['website_id', 'url', 'c_name', 'country', 'language', 'remark', 'developer', 'start_time',
+                   'news_amount']
 
 
 def daily():
@@ -74,6 +75,7 @@ def daily():
     data.append([sum])
     return data
 
+
 def all():
     '''
     累计数据生成逻辑
@@ -93,11 +95,13 @@ def all():
     data.append([sum])
     return data
 
+
 def annual():
     '''
     年度统计表格生成逻辑
     '''
     pass
+
 
 def run():
     global time_yesterday
