584de2bc15ffb25b4227e43bfaff779ac9938d17
Lab_ldqsss <734200940@qq.com>
2021-11-10 15:50:36 +0800

    cookie solved

--- crawler/spiders/aseanorg.py
+++ crawler/spiders/aseanorg.py
@@ -1,6 +1,5 @@
-import requests
 from bs4 import BeautifulSoup
-from scrapy.http import headers
+from utils.date_util import DateUtil
 from crawler.spiders import BaseSpider
 from crawler.items import *
 from scrapy.http.request import Request
@@ -26,7 +25,8 @@ class AseanorgSpider(BaseSpider):
 
     def start_requests(self):
         for i in self.start_urls:
-            yield Request(url=i,meta={'Headers':MOZILLA_HEADER})
+            category1 = i.split('/')[-2]
+            yield Request(url=i,meta={'Headers':MOZILLA_HEADER,"category1":category1})
 
     def getCookie(self, response):
         # 用re把js代码扣下来
@@ -47,58 +47,40 @@ class AseanorgSpider(BaseSpider):
             header["cookie"] = self.getCookie(response)
             # 往request传请求头的方式是直接给meta的Headers传个列表，里面是要修改的请求头字段
             # 如果Headers里没有user-agent字段会刷新ua，传了就不会刷新而是用你传进去的那个
-            yield Request(url=response.url,meta={'Headers':header},dont_filter=True)
+            yield Request(url=response.url, meta={'Headers': header,"category1":response.meta['category1']}, dont_filter=True)
         else:
             soup = BeautifulSoup(response.text, 'html.parser')
-            articles = soup.select('div.elementor-widget-container > article > a')
+            articles = soup.select('article > a')
             for article in articles:
                 article_url = article.get('href')
                 # 后面这些也是一样
-                yield Request(url=article_url, callback=self.parse_item)
+                # print(article_url)
+                yield Request(url=article_url, callback=self.parse_item, meta={'Headers':response.meta['Headers'],'category1':response.meta['category1']})
             next_page = soup.select_one('.page-numbers.next').get('href')
-        # yield Request(url=next_page, meta=response.meta, callback=self.parse_page, headers=hd,dont_filter=True)
+            yield Request(url=next_page, meta={'Headers': response.meta['Headers'],'category1':response.meta['category1']}, callback=self.parse_page, dont_filter=True)
 
     def parse_page(self, response):
-        print("parseeeeeeeeeeeeeeeeee")
-        print(response.text)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     articles = soup.select('div.elementor-widget-container > article > a')
-    #     hd = self.headers
-    #     hd['referer'] = response.url
-    #     for article in articles:
-    #         article_url = article.get('href')
-    #         yield Request(url=article_url, callback=self.parse_item,headers=hd,dont_filter=True)
-    #     next_page = soup.select_one('.page-numbers.next').get('href')
-    #     print(next_page)
-    #     yield Request(url=next_page, meta=response.meta, callback=self.parse_page, headers=hd,dont_filter=True)
-    #
+        soup = BeautifulSoup(response.text, 'html.parser')
+        articles = soup.select('article > a')
+        for article in articles:
+            article_url = article.get('href')
+            yield Request(url=article_url, callback=self.parse_item,meta=response.meta,dont_filter=True)
+        next_page = soup.select_one('.page-numbers.next').get('href')
+        yield Request(url=next_page, meta=response.meta, callback=self.parse_page,dont_filter=True)
+
     def parse_item(self, response):
-        print(response.text)
-        # if (re.findall(r'<title>(.*)</title>', response.text)[0] == 'You are being redirected...'):
-        #     print("parse_itemmmmmmmmmmmmmmmmmmmmm")
-        #     cookie = self.getCookie(response)
-        #     hd = self.headers
-        #     hd['cookie'] = cookie
-        #     yield Request(url=response.url, headers=hd, callback=self.parse_items,dont_filter=True)
-    #
-    # def parse_items(self, response):
-    #     print("parse_itemmmmmmmmmmmmmmmmmmmmm")
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     item = NewsItem()
-    #     item['category1'] = soup.select_one(
-    #         '.esoup.selecrlementor-widget-container div.breadcrumb > a:nth-child(3)').text
-    #     item['title'] = soup.select_one(
-    #         'div.elementor-element.elementor-element-b369dd8.elementor-widget.elementor-widget-theme-post-title.elementor-page-title.elementor-widget-heading > div > h2').text.strip()
-    #     tt = soup.select_one(
-    #         'div.elementor-element.elementor-element-f86aebc.elementor-widget.elementor-widget-post-info > div > ul > li > a > span').text.strip().replace(
-    #         ',', ' ').split(' ')
-    #     item['pub_time'] = "{}-{}-{}".format(tt[2], month[tt[2]], tt[0]) + ' 00:00:00'
-    #     item['images'] = [img.get('src') for img in
-    #                       soup.select(' div.elementor-container.elementor-column-gap-wide .elementor-image img')]
-    #     item['body'] = '\n'.join([paragraph.text.strip()
-    #                               for paragraph in soup.select(
-    #             'section.elementor-section.elementor-top-section.elementor-element.elementor-element-8e4d6ae.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element.elementor-element-1113e65 > div > div > div > div > div > div > div > section > div > div > div > div > div > div > div.elementor-widget-container > div.elementor-text-editor elementor-clearfix p')
-    #                               if
-    #                               paragraph.text != '' and paragraph.text != ' '])
-    #     item['abstract'] = item['body'].split('\n')[0]
-    #     yield item
+        soup = BeautifulSoup(response.text, 'html.parser')
+        item = NewsItem()
+        item['category1'] = response.meta['category1']
+        item['title'] = soup.select_one('div > h2').text.strip()
+        try:
+            tt = soup.find(class_='elementor-icon-list-item elementor-repeater-item-ca9fc7d elementor-inline-item').find('a').get('href').split('/')
+            item['pub_time'] = "{}-{}-{} 00:00:00".format(tt[-4],tt[-3],tt[-2])
+        except:
+            item['pub_time'] = DateUtil.time_now_formate()
+        item['images'] = [img.get('src') for img in soup.select('.elementor-section-wrap .elementor-image img')]
+        item['body'] = '\n'.join([paragraph.text.strip()
+                                  for paragraph in soup.select('.elementor-widget-container p')
+                                    if paragraph.text != '' and paragraph.text != ' '])
+        item['abstract'] = item['body'].split('\n')[0]
+        yield item
