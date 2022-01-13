c4324a111740e9379de28bd6fbecc4f4d457687c
ldqsss <734200940@qq.com>
2022-01-13 17:59:55 +0800

    check wei pengfeng

--- crawler/pass/pids_gov_ph.py
+++ crawler/pass/pids_gov_ph.py
@@ -3,43 +3,28 @@ import socket
 import common.date
 from crawler.spiders import BaseSpider
 # 此文件包含的头文件不要修改
-import scrapy
-from utils.util_old import *
 from crawler.items import *
 from bs4 import BeautifulSoup
 from scrapy.http import Request, Response
 from utils import date_util
-
-from datetime import datetime
-import time
-import re
-import requests
 
 # author:魏芃枫
 
 
 class Pids(BaseSpider):
     name = 'pids_gov_ph'
-    start_urls = ['https://pids.gov.ph/press-releases']
+    start_urls = [f'https://pids.gov.ph/press-releases?year={str(i)}' for i in range(2013, 2022)]
     website_id = 1256  # 网站的id(必填)
     language_id = 1866  # 所用语言的id
 
-    headers = {
-        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-
     def parse(self, response):  # 获取不同年份下的不同页
-        socket.setdefaulttimeout(30)
-        for i in range(2013, 2022):
-            head_url = "https://pids.gov.ph/press-releases?year="+str(i)
-            response_page = requests.get(head_url)
-            soup = BeautifulSoup(response_page.text, 'html.parser')
-            page_list = soup.select(".paginate-wrapper>ul>li")
-            page_num = int(len(page_list) / 2 - 4) # 根据li标签数量计算得页数
-            for j in range(1, page_num+1):
-                url = head_url+"&pagenum="+str(j)
-                yield Request(url, callback=self.parse_page)
+        head_url = response.url
+        soup = BeautifulSoup(response.text, 'html.parser')
+        page_list = soup.select(".paginate-wrapper>ul>li")
+        page_num = int(len(page_list) / 2 - 4) # 根据li标签数量计算得页数
+        for j in range(1, page_num+1):
+            url = head_url+"&pagenum="+str(j)
+            yield Request(url, callback=self.parse_page)
 
     def parse_page(self, response):
         socket.setdefaulttimeout(30)
@@ -64,15 +49,13 @@ class Pids(BaseSpider):
                 continue
 
     def parse_detail(self, response):
-        socket.setdefaulttimeout(30)
         soup = BeautifulSoup(response.text, 'html.parser')
-        body = soup.find(attrs={"class": 'large-8 columns page-content-column'}).text
         item = NewsItem()
         item['title'] = response.meta['title']
         item['pub_time'] = response.meta['pub_time']
-        item['body'] = body
-        item['abstract'] = body
+        item['body'] = soup.find(attrs={"class": 'large-8 columns page-content-column'}).strip()
+        item['abstract'] = soup.find(attrs={"class": 'large-8 columns page-content-column'}).split('\n')[0]
         item['category1'] = None
         item['category2'] = None
         item['images'] = None
-        return item
+        yield item
