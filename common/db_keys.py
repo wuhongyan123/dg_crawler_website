3b0d7627edfc917818701123d739a0ac7ca14379
modifyGB <2782299413@qq.com>
2022-02-28 22:33:40 +0800

    commit

--- common/db_keys.py
+++ common/db_keys.py
@@ -1,3 +1,11 @@
+'''
+Description: file description
+Version: 1.0
+Autor: Renhetian
+Date: 2021-10-11 11:39:50
+LastEditors: Renhetian
+LastEditTime: 2022-02-28 22:13:37
+'''
 # encoding: utf-8
 
 # dg_crawler_website数据库news表字段
@@ -22,4 +30,17 @@ WEBSITE_HTML_KEYS = (
     'md5',
     'html',
     'create_time'
+)
+
+# common_website数据库development表字段
+WEBSITE_DEVELOPMENT_KEYS = ( 
+    'website_id',
+    'name',
+    'proxy',
+    'level',
+    'is_open',
+    'remark',
+    'is_http',
+    'start_time',
+    'run_time'
 )
--- scripts/develop.py
+++ scripts/develop.py
@@ -4,15 +4,39 @@ Version: 1.0
 Autor: Renhetian
 Date: 2022-02-28 14:44:59
 LastEditors: Renhetian
-LastEditTime: 2022-02-28 14:47:31
+LastEditTime: 2022-02-28 22:32:18
 '''
 
+import sys
+import inspect
+import pkgutil
 from fire import Fire
 
-paths = ['crawler/pass/']
+from crawler.spiders import BaseSpider
+from utils.date_util import DateUtil
+from libs.mysql import Mysql
+from config.db_config import COMMON_WEBSITE_CONFIG
+from common.db_keys import WEBSITE_DEVELOPMENT_KEYS
+
+paths = ['crawler/passed']
+
+sql1 = 'SELECT website_id FROM development'
+sql2 = 'UPDATE website SET `status`= 1, start_time={} WHERE website_id={}'
 
 def run():
-    pass
+    sql = Mysql(COMMON_WEBSITE_CONFIG)
+    spider_list = []
+    website_id_list = [i[0] for i in sql.select(sql1)]
+    for _, modname, _ in pkgutil.walk_packages(path=paths, prefix='crawler.passed.'):
+        exec('from ' + modname + ' import *')
+        for _, class_ in inspect.getmembers(sys.modules[modname], inspect.isclass):
+            if issubclass(class_, BaseSpider) and class_ != BaseSpider:
+                if class_.website_id in website_id_list:
+                    continue
+                sql.insert('development', WEBSITE_DEVELOPMENT_KEYS, 
+                [class_.website_id, class_.name, '00', 2, 1, '', 0, 
+                DateUtil.time_now_formate(), DateUtil.time_now_formate()])
+                sql.execute(sql2.format(DateUtil.time_now_formate(), class_.website_id))
 
 # python -m scripts.develop
 if __name__ == "__main__":
