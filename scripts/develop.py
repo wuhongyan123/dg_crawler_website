ae46a305b40ac9271824e403f65cd9b11ecbc358
modifyGB <2782299413@qq.com>
2022-02-28 22:43:37 +0800

    commit

--- scripts/develop.py
+++ scripts/develop.py
@@ -4,7 +4,7 @@ Version: 1.0
 Autor: Renhetian
 Date: 2022-02-28 14:44:59
 LastEditors: Renhetian
-LastEditTime: 2022-02-28 22:32:18
+LastEditTime: 2022-02-28 22:41:30
 '''
 
 import sys
@@ -21,11 +21,10 @@ from common.db_keys import WEBSITE_DEVELOPMENT_KEYS
 paths = ['crawler/passed']
 
 sql1 = 'SELECT website_id FROM development'
-sql2 = 'UPDATE website SET `status`= 1, start_time={} WHERE website_id={}'
+sql2 = 'UPDATE website SET `status`= 1, start_time="{}" WHERE id={}'
 
 def run():
     sql = Mysql(COMMON_WEBSITE_CONFIG)
-    spider_list = []
     website_id_list = [i[0] for i in sql.select(sql1)]
     for _, modname, _ in pkgutil.walk_packages(path=paths, prefix='crawler.passed.'):
         exec('from ' + modname + ' import *')
@@ -37,6 +36,7 @@ def run():
                 [class_.website_id, class_.name, '00', 2, 1, '', 0, 
                 DateUtil.time_now_formate(), DateUtil.time_now_formate()])
                 sql.execute(sql2.format(DateUtil.time_now_formate(), class_.website_id))
+                print('爬虫已录入 ==> id:{}'.format(class_.website_id))
 
 # python -m scripts.develop
 if __name__ == "__main__":
